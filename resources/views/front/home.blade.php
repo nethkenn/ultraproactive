3aa475175f8db8cc5368464477f39ef272761c7c
Edward Dc. Guevarra <edwardguevarra2003@gmail.com>
2015-06-30 12:45:50 +0800

    frontend

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,4 +1,541 @@
 @extends('front.layout')
 @section('content')
-HELLO WORLD
+<div class="topicons">
+<a href="http://facebook.com" target="_blank" class="iconfacebook"></a>
+<a href="#" class="icontwitter"></a>
+<a href="#" class="iconlinkedin"></a>
+</div>
+<header class="topwrapwrapper">
+<section class="topwrap">
+<div class="sequence-theme">
+<img class="sequence-prev" src="/resources/assets/slider_style/images/prev_yellow.png" alt="Previous Frame" />
+<img class="sequence-next" src="/resources/assets/slider_style/images/next_yellow.png" alt="Next Frame" />
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div id="sequence">
+<ul class="sequence-canvas">
+<li>
+<h2 class="title">OUR NEW HEROES</h2>
+<h3 class="subtitle">OVERSEAS FILIPINO WORKER</h3>
+</li>
+
+</ul>
+<div class="sequence-pagination">
+<div class="paginationslider"></div>
+<div class="paginationslider"></div>
+<div class="paginationslider"></div>
+</div>
+</div></div>
+</div></div></div>
+<div class="topbuttonwrapper">
+<a href="#1" id="topbutton"></a>
+</div>
+</section>
+</header>
+<div class="wrapperall">
+<section class="wrapper100percent section1">
+<div id="1" class="sectionwrapper">
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div class="mainheadlinewrapper">
+<div class="mainheadline">
+<h2> Mobile App</h2>
+</div>
+</div>
+</div><!--/col-lg-12 end-->
+</div><!--/row end-->
+<div class="row">
+<div class="col-lg-12 maintext">
+<p>
+Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima.
+</p>
+</div><!--/col-lg-12 end-->
+</div><!--/row end-->
+<div class="row approwwrapper">
+<div class="col-lg-4 apptextleft">
+<div class="apptextleftinner">
+<h4>Take a look at our</h4>
+<h3>Duality App</h3>
+</div><!--/apptextleftinner end-->
+</div><!--/col-lg-4 end-->
+<div class="col-lg-4">
+<div class="appwrapper1">
+<div class="appwrapper">
+<div class="appinner">
+<img src="/resources/assets/slider_style/images/app.jpg" id="app" alt=""/>
+</div>
+</div>
+</div>
+</div><!--/col-lg-4 end-->
+<div class="col-lg-4 apptextright">
+<div class="apptextrightone">
+<h3>Headline One</h3>
+<p>
+Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem.
+Mirum est notare quam littera gothica
+</p>
+</div><!--/apptextrightone end-->
+<div class="apptextrighttwo">
+<h3>Headline Two</h3>
+<p>
+Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem.
+Mirum est notare quam littera gothica
+</p>
+</div><!--/apptextrightone end-->
+<div class="apptextrightthree">
+<h3>Headline Three</h3>
+<p>
+Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem.
+Mirum est notare quam littera gothica
+</p>
+</div><!--/apptextrightone end-->
+<div class="apptextrightfour">
+<h3>Headline Four</h3>
+<p>
+Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem.
+Mirum est notare quam littera gothica
+</p>
+</div><!--/apptextrightone end-->
+</div><!--/col-lg-4 end-->
+</div><!--/row end-->
+</div><!--/container end-->
+</section><!--/one section end-->
+<section class="blackpart1">
+<h3>This is <span>call to action</span> box</h3>
+<div class="calltoactionwrapper">
+<a href="#" id="calltoaction">Buy now!</a>
+</div>
+</section><!--/blackpart1 end-->
+<section class="wrapper100percent section1">
+<div id="2" class="sectionwrapper">
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div class="mainheadlinewrapper">
+<div class="mainheadline">
+<h2>Services</h2>
+</div>
+</div>
+</div><!--/col-lg-12 end-->
+</div><!--/row end-->
+<div class="row">
+<div class="col-lg-12 maintext">
+<p>
+Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima.
+</p>
+</div><!--/col-lg-12 end-->
+</div><!--/row end-->
+<div class="row">
+<div class="col-lg-3 onecolumnservice">
+<img src="/resources/assets/slider_style/images/service1_yellow.png" alt=""/>
+<h4>Service yellow</h4>
+<p>
+Anteposuerit litte quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacul.
+</p>
+<a href="#">continue</a>
+</div><!--/onecolumnservice end-->
+<div class="col-lg-3 onecolumnservice">
+<img src="/resources/assets/slider_style/images/service2_yellow.png" alt=""/>
+<h4>Service red</h4>
+<p>
+Anteposuerit litte quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacul.
+</p>
+<a href="#">continue</a>
+</div><!--/onecolumnservice end-->
+<div class="col-lg-3 onecolumnservice">
+<img src="/resources/assets/slider_style/images/service3_yellow.png" alt=""/>
+<h4>Service green</h4>
+<p>
+Anteposuerit litte quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacul.
+</p>
+<a href="#">continue</a>
+</div><!--/onecolumnservice end-->
+<div class="col-lg-3 onecolumnservice">
+<img src="/resources/assets/slider_style/images/service4_yellow.png" alt=""/>
+<h4>Service blue</h4>
+<p>
+Anteposuerit litte quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacul.
+</p>
+<a href="#">continue</a>
+</div><!--/onecolumnservice end-->
+</div><!--/row end-->
+</div><!--/container end-->
+</section><!--/one section end-->
+<section class="blackpart2">
+<div id="3" class="sectionwrapper">
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div class="mainheadlinewrapperblack">
+<div class="mainheadlineblack">
+<h2>Portfolio</h2>
+</div>
+</div>
+</div><!--/col-lg-12 end-->
+</div><!--/row end-->
+</div><!--/container end-->
+<div class="wrapper100percent">
+<div>
+<div id="options" class="clearfix">
+<ul id="filters" class="option-set clearfix" data-option-key="filter">
+<li><a href="#filter" data-option-value="*" class="selected">all</a></li>
+<li><a href="#filter" data-option-value=".filterone">web design</a></li>
+<li><a href="#filter" data-option-value=".filtertwo">web development</a></li>
+<li><a href="#filter" data-option-value=".filterthree">photography</a></li>
+</ul> </div>
+<ul id="portfoliocontainer" class="grid cs-style-3">
+<li class="element filtertwo">
+<figure>
+<img src="/resources/assets/slider_style/images/isotope1.jpg" alt="img01">
+<figcaption>
+<h3>Headline</h3>
+<a class='iframe group4' href="/resources/assets/slider_style/additionalpage.html">Take a look</a>
+</figcaption>
+</figure>
+</li>
+<li class="element filterone">
+<figure>
+<img src="/resources/assets/slider_style/images/isotope2.jpg" alt="img01">
+<figcaption>
+<h3>Headline</h3>
+<a class='iframe group4' href="/resources/assets/slider_style/additionalpage.html">Take a look</a>
+</figcaption>
+</figure>
+</li>
+<li class="element filterthree">
+<figure>
+<img src="/resources/assets/slider_style/images/isotope3.jpg" alt="img01">
+<figcaption>
+<h3>Headline</h3>
+<a class='iframe group4' href="/resources/assets/slider_style/additionalpage.html">Take a look</a>
+</figcaption>
+</figure>
+</li>
+<li class="element filtertwo">
+<figure>
+<img src="/resources/assets/slider_style/images/isotope4.jpg" alt="img01">
+<figcaption>
+<h3>Headline</h3>
+<a class='iframe group4' href="/resources/assets/slider_style/additionalpage.html">Take a look</a>
+</figcaption>
+</figure>
+</li>
+<li class="element filterthree">
+<figure>
+<img src="/resources/assets/slider_style/images/isotope5.jpg" alt="img01">
+<figcaption>
+<h3>Headline</h3>
+<a class='iframe group4' href="/resources/assets/slider_style/additionalpage.html">Take a look</a>
+</figcaption>
+</figure>
+</li>
+<li class="element filterone">
+<figure>
+<img src="/resources/assets/slider_style/images/isotope6.jpg" alt="img01">
+<figcaption>
+<h3>Headline</h3>
+<a class='iframe group4' href="/resources/assets/slider_style/additionalpage.html">Take a look</a>
+</figcaption>
+</figure>
+</li>
+<li class="element filterthree">
+<figure>
+<img src="/resources/assets/slider_style/images/isotope7.jpg" alt="img01">
+<figcaption>
+<h3>Headline</h3>
+<a class='iframe group4' href="/resources/assets/slider_style/additionalpage.html">Take a look</a>
+</figcaption>
+</figure>
+</li>
+<li class="element filterone">
+<figure>
+<img src="/resources/assets/slider_style/images/isotope8.jpg" alt="img01">
+<figcaption>
+<h3>Headline</h3>
+<a class='iframe group4' href="/resources/assets/slider_style/additionalpage.html">Take a look</a>
+</figcaption>
+</figure>
+</li>
+<li class="element filterthree">
+<figure>
+<img src="/resources/assets/slider_style/images/isotope1.jpg" alt="img01">
+<figcaption>
+<h3>Headline</h3>
+<a class='iframe group4' href="/resources/assets/slider_style/additionalpage.html">Take a look</a>
+</figcaption>
+</figure>
+</li>
+</ul>
+</div><!--/container end-->
+</div><!--/wrapper100percent end-->
+</section><!--/blackpart2 end-->
+<section class="wrapper100percent section1">
+<div id="4" class="sectionwrapper">
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div class="mainheadlinewrapper">
+<div class="mainheadline">
+<h2>Team</h2>
+</div>
+</div>
+</div><!--/col-lg-12 end-->
+</div><!--/row end-->
+<div class="row">
+<div class="col-lg-12 maintext">
+<p>
+Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima.
+</p>
+</div><!--/col-lg-12 end-->
+</div><!--/row end-->
+<div class="row">
+<div class="col-lg-4 onecolumnteam">
+<img src="/resources/assets/slider_style/images/team1.jpg" alt=""/>
+<div class="onecolumnteamtext">
+<h4>John Doe</h4>
+<p>
+Web Developer
+</p>
+</div>
+<div class="teamicons">
+<a href="#" class="iconfacebook"></a>
+<a href="#" class="icontwitter"></a>
+<a href="#" class="iconlinkedin"></a>
+</div>
+</div><!--/onecolumnservice end-->
+<div class="col-lg-4 onecolumnteam">
+<img src="/resources/assets/slider_style/images/team2.jpg" alt=""/>
+<div class="onecolumnteamtext">
+<h4>Jane Doe</h4>
+<p>
+General Manager
+</p>
+</div>
+<div class="teamicons">
+<a href="#" class="iconfacebook"></a>
+<a href="#" class="icontwitter"></a>
+<a href="#" class="iconlinkedin"></a>
+</div>
+</div><!--/onecolumnservice end-->
+<div class="col-lg-4 onecolumnteam">
+<img src="/resources/assets/slider_style/images/team1.jpg" alt=""/>
+<div class="onecolumnteamtext">
+<h4>John Doe</h4>
+<p>
+Web Designer
+</p>
+</div>
+<div class="teamicons">
+<a href="#" class="iconfacebook"></a>
+<a href="#" class="icontwitter"></a>
+<a href="#" class="iconlinkedin"></a>
+</div>
+</div><!--/onecolumnservice end-->
+</div><!--/row end-->
+<div class="row">
+<div class="col-lg-12">
+<div class="mainheadlinewrapper">
+<div class="mainheadline">
+<h2></h2>
+</div>
+</div>
+</div><!--/col-lg-12 end-->
+</div><!--/row end-->
+<div class="row">
+<div class="teamskills">
+<div class="wrap">
+<div class="col-lg-3">
+<p>HTML3</p>
+<div class="progress-radial progress-90">
+<div class="overlay"><p><span>90%</span></p>
+</div>
+</div>
+</div>
+</div>
+<div class="wrap">
+<div class="col-lg-3">
+<p>CSS3</p>
+<div class="progress-radial progress-80">
+<div class="overlay"><p><span>80%</span></p></div>
+</div>
+</div>
+</div>
+<div class="wrap">
+<div class="col-lg-3">
+<p>jQuery</p>
+<div class="progress-radial progress-50">
+<div class="overlay"><p><span>50%</span></p></div>
+</div>
+</div>
+</div>
+<div class="wrap">
+<div class="col-lg-3">
+<p>SEO</p>
+<div class="progress-radial progress-40">
+<div class="overlay"><p><span>40%</span></p></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div><!--/container end-->
+</section><!--/one section end-->
+<section class="blackpart3">
+<section class="bxwrap">
+<ul id="bxslider">
+<li>
+<div class="textbx">
+<p class="subtitle2">"Lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor."
+<img src="/resources/assets/slider_style/images/testimonials.jpg" alt=""/>
+<p class="subtitle1">John Doe / Web Designer
+</p>
+</div> <!--/textbx end-->
+</li>
+<li>
+<div class="textbx">
+<p class="subtitle2">"Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum. Investigationes demonstraverunt lectores legere me lius."
+<img src="/resources/assets/slider_style/images/testimonials.jpg" alt=""/>
+<p class="subtitle1">John Doe / General Manager
+</p>
+</div> <!--/textbx end-->
+</li>
+<li>
+<div class="textbx">
+<p class="subtitle2">"Lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem"
+<img src="/resources/assets/slider_style/images/testimonials.jpg" alt=""/>
+<p class="subtitle1">John Doe / Manager
+</p>
+</div> <!--/textbx end-->
+</li>
+</ul>
+</section>
+</section><!--/blackpart3 end-->
+<section class="wrapper100percent section1">
+<div id="5" class="sectionwrapper">
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div class="mainheadlinewrapper">
+<div class="mainheadline">
+<h2>NEWS AND UPDATES</h2>
+</div>
+</div>
+</div><!--/col-lg-12 end-->
+</div><!--/row end-->
+<div class="row">
+<div class="col-lg-12 maintext">
+<p>
+Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima.
+</p>
+</div><!--/col-lg-12 end-->
+</div><!--/row end-->
+<div class="row">
+<div class="col-lg-4 onecolumnblog">
+<img src="/resources/assets/slider_style/images/blog1.jpg" alt=""/>
+<div class="onecolumnblogtext">
+<h4>Headline</h4>
+<h6>
+15.March 2014.
+</h6>
+</div>
+<p>
+Anteposuerit litte quam nunc putamus parum claram, litterarum formas humanitatis per seacul anteposuerit litterarum formas humanitatis per seacul.
+</p>
+<a href="/resources/assets/slider_style/#">continue</a>
+</div><!--/onecolumnblog end-->
+<div class="col-lg-4 onecolumnblog">
+<img src="/resources/assets/slider_style/images/blog2.jpg" alt=""/>
+<div class="onecolumnblogtext">
+<h4>Headline</h4>
+<h6>
+17.March 2014.
+</h6>
+</div>
+<p>
+Anteposuerit litte quam nunc putamus parum claram, litterarum formas humanitatis per seacul anteposuerit litterarum formas humanitatis per seacul.
+</p>
+<a href="/resources/assets/slider_style/#">continue</a>
+</div><!--/onecolumnblog end-->
+<div class="col-lg-4 onecolumnblog">
+<img src="/resources/assets/slider_style/images/blog3.jpg" alt=""/>
+<div class="onecolumnblogtext">
+<h4>Headline</h4>
+<h6>
+18.March 2014.
+</h6>
+</div>
+<p>
+Anteposuerit litte quam nunc putamus parum claram, litterarum formas humanitatis per seacul anteposuerit litterarum formas humanitatis per seacul.
+</p>
+<a href="/resources/assets/slider_style/#">continue</a>
+</div><!--/onecolumnblog end-->
+</div><!--/row end-->
+</div><!--/container end-->
+</section><!--/one section end-->
+<section class="blackpart4">
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<img src="/resources/assets/slider_style/images/hireusleftimg_yellow.png" class="hireusleftimg" alt=""/>
+<img src="/resources/assets/slider_style/images/hireus.png" class="hireusimg" alt=""/>
+<div class="hireuswraper">
+<a href="/resources/assets/slider_style/#" id="hireus">Hire us!</a>
+</div>
+</div></div></div>
+</section><!--/blackpart4 end-->
+<section class="blackpart4inner">
+<img src="/resources/assets/slider_style/images/contactarrow.png" class="contactarrow" alt=""/>
+</section><!--/blackpart4inner end-->
+<section class="wrapper100percent section1">
+<div id="6" class="sectionwrapper">
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div class="mainheadlinewrapper">
+<div class="mainheadline">
+<h2>Send your message</h2>
+</div>
+</div>
+</div><!--/col-lg-12 end-->
+</div><!--/row end-->
+<div class="row">
+<div class="col-lg-12">
+<!--/comment-form-->
+<div>
+<form method="post" action="http://rayoflightthemes.com/htmltemplates/duality_html_template/slider_style/sendEmail.php">
+<div>
+<div id="main">
+<div class="contact1">
+<p><input type="text" placeholder="your name" name="name" id="name"
+class="commentfield" /></p> </div>
+<div class="contact2">
+<p> <input type="text" placeholder="your e-mail adress" name="email" id="email"
+class="commentfield" /></p> </div>
+<p><textarea name="comments" placeholder="message" id="comments"
+rows="12" cols="5" class="textarea"></textarea></p>
+<div class="buttoncontactwrapper">
+<p><input type="submit"  name="submit"  class="buttoncontact" id="submit" value="Send"/>
+</p></div>
+<ul id="response"></ul>
+</div></div>
+</form> </div></div>
+<!--/comment-form-end-->
+</div>
+</div>
+</section><!--/one section end-->
+<section class="mapwrapper">
+<a href="/resources/assets/slider_style/#" class="mapbutton">map</a>
+<div id="map">
+<iframe src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=New+York,+NY,+United+States&amp;aq=1&amp;oq=new+your&amp;sll=37.0625,-95.677068&amp;sspn=56.243791,114.169922&amp;ie=UTF8&amp;hq=&amp;hnear=New+York&amp;t=m&amp;z=10&amp;ll=40.714353,-74.005973&amp;output=embed"></iframe><br /><small><a href="/resources/assets/slider_style/https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=New+York,+NY,+United+States&amp;aq=1&amp;oq=new+your&amp;sll=37.0625,-95.677068&amp;sspn=56.243791,114.169922&amp;ie=UTF8&amp;hq=&amp;hnear=New+York&amp;t=m&amp;z=10&amp;ll=40.714353,-74.005973">View Larger Map</a></small>
+</div>
+</section><!--/map end-->
+
 @endsection
