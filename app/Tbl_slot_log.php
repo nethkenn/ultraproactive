8baefb5b3094a1ce482cd6080f9637835d873544
Edward Dc. Guevarra <edwardguevarra2003@gmail.com>
2015-07-27 10:16:48 +0800

    tbl_pamore

--- app/Tbl_slot_log.php
+++ app/Tbl_slot_log.php
@@ -5,6 +5,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tbl_slot_log extends Model
 {
-	protected $table = 'Tbl_slot_log';
+	protected $table = 'tbl_slot_log';
 	protected $primaryKey = "slot_log_id";
 }
