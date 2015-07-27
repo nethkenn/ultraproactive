d16c0ff778665f9e7c7a69ec0979b81f015d7e45
mark ponce <primiaph@gmail.com>
2015-07-27 10:44:03 +0800

    7-27-15 10:43

--- database/migrations/2015_07_27_094904_create_tbl_membership_code_sale.php
+++ database/migrations/2015_07_27_094904_create_tbl_membership_code_sale.php
@@ -14,14 +14,22 @@ class CreateTblMembershipCodeSale extends Migration
     {
         Schema::create('tbl_membership_code_sale', function (Blueprint $table) {
             $table->increments('membershipcode_or_num');
+            $table->string('membershipcode_or_code');
             $table->integer('sold_to')->unsigned()->nullable();
             $table->integer('generated_by')->unsigned()->nullable();
+            $table->string('generated_by_name');
+
             $table->double('total_amount');
             $table->timestamps();
 
 
             $table->foreign('sold_to')
               ->references('account_id')->on('tbl_account')
+              ->onDelete('cascade');
+
+
+            $table->foreign('sold_to')
+              ->references('admin_id')->on('tbl_admin')
               ->onDelete('cascade');
         });
     }
