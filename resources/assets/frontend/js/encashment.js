59aae1310bba3264759528900ba3e3b7b8cc8f1b
Primia IT Solutions <primiaph@gmail.com>
2015-08-04 02:52:30 -0400

    2:52pm

--- resources/assets/frontend/js/encashment.js
+++ resources/assets/frontend/js/encashment.js
@@ -16,26 +16,36 @@ function encashment()
 		initialize();
 		check_value();
 		country = jQuery.parseJSON($(".forhidden").val());
+
 	}
 
 
 	function initialize()
-	{
+	{	
+
+
 		$("#clickencash").click(function()
 		{	
+
+
+			// 		 var test = ;
+		 // alert(test);
 			deduction = 0;
-			var one = $("#amount").val();
+
+			var one = (parseFloat($("#amount").val())).formatMoney(2); 
 			var two = $(".max").attr('val');
 			var four = 0;
 			var five = 0;
 			$("#enc").val($("#typeencashment").val());
 			$("#two").val(one);
 			$("#one").val(two);
-			$("#three").val(parseInt(two)-parseInt(one));
+			$("#three").val((two-one).formatMoney(2));
 			$("#four").val(four);
 			$("#five").val(five);
-			$("#six").val(parseInt($("#three").val())-(parseInt(four)+parseInt(five)))
-			compute_country();
+			
+			var recievable = compute_recievable(one)
+			$("#six").val((recievable).formatMoney(2));
+			// console.log(recievable);
 			var inst = $('[data-remodal-id=encashment]').remodal();
           	inst.open(); 
           	check_value();
@@ -46,10 +56,12 @@ function encashment()
           	inst.open(); 
 		});
 	}
-	function compute_country()
+	function compute_recievable(encashment)
 	{
+
+		var f_total = 0;
 		$.each( country.forjson, function( key, value ) 
-        {
+        {	
         	var total2 = 0;
         	if(value.percent == 1)
         	{
@@ -61,11 +73,20 @@ function encashment()
         		deduction = deduction + value.deduction_amount;
         		total2 = value.deduction_amount;
         	}
-        	$("#"+value.deduction_id).val(total2);
+
+        	$("#"+value.deduction_id).val((parseFloat(total2)).formatMoney(2));
+
+        	f_total = parseFloat(total2) + parseFloat(f_total);
         });
 
-        $("#six").val(parseInt($("#two").val())-deduction);
+
+        // alert(f_total);
+        return parseFloat(encashment) - parseFloat(f_total);
+        
+
+        // $("#six").val(parseInt($("#two").val()) - parseInt(deduction));
 	}
+
 	function check_value()
 	{
         if(parseInt($("#six").val()) >= 0 && parseInt($("#three").val()) >= 0)
@@ -78,5 +99,5 @@ function encashment()
         }
 
 	}
-}
 
+}
