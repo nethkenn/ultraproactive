6b0d3589fe396560e049b4d8f723d6a87b16b81b
mark ponce <primiaph@gmail.com>
2015-08-05 17:27:18 +0800

    asdsa

--- database/migrations/2015_08_05_172309_create_tbl_stockist_type.php
+++ database/migrations/2015_08_05_172309_create_tbl_stockist_type.php
@@ -15,10 +15,10 @@ class CreateTblStockistType extends Migration
         Schema::create('tbl_stockist_type', function (Blueprint $table) {
             $table->increments('stockist_type_id');
             $table->string('stockist_type_name');
-            $table->double('stockist_type_discount');
-            $table->double('stockist_type_package_discount');
-            $table->integer('stockist_type_minimum_order');
-            $table->tinyInteger('stockist_type_minimum_order');
+            $table->double('stockist_type_discount')->default(0);
+            $table->double('stockist_type_package_discount')->default(0);
+            $table->integer('stockist_type_minimum_order')->default(0);
+            $table->tinyInteger('stockist_type_minimum_order')->default(0);
         });
     }
 
