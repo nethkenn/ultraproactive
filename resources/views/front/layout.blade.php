d343a20c75dc0957693366716f9925a117165118
Edward Dc. Guevarra <edwardguevarra2003@gmail.com>
2015-11-30 11:54:37 +0800

    fix script

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -218,8 +218,6 @@
 	</footer>
 	<script type="text/javascript" src="/resources/assets/path/js/jquery.js"></script>
 	<script type="text/javascript" src="/resources/assets/path/js/jquery-ui.min.js"></script>
-	<script type="text/javascript" src="/resources/assets/path/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
-	<script type="text/javascript" src="/resources/assets/path/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
 	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&amp;ver=4.0.1"></script>
 	<script type="text/javascript" src="/resources/assets/path/js/scripts.js"></script>
 	<script type="text/javascript" src="/resources/assets/path/js/custom.js"></script>
